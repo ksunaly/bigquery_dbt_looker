4ef5a6ed02ae2db02a057ef5e3acbf0e00589f0c
ksunaly <ksunaly03@gmail.com>
2024-10-19 21:33:17 -0700

    Add files via upload

--- Technical_task/dbt/models/gold/daily_product_logistics.sql
+++ Technical_task/dbt/models/gold/daily_product_logistics.sql
@@ -1,40 +1,67 @@
 -- dbt Model File
 
-{{ config(materialized='table') }}
+{{ config(materialized='incremental', unique_key='orderid') }}
+
 
 with
 
 orders as (
     -- a raw transactional fact table for orders being placed i.e. created
-    select * from fivetran.ecommerce.orders
+    select 
+       orderid, ----added column names instead of *
+       productid,
+       customerid,
+       createdat
+    from {{ ref('bronze_orders') }}  --table is not new, using ref 
+    {% if is_incremental() %}
+       where createdat > (select max(createdat) from {{ this }})  -- Fetch only updated records
+    {% endif %}
 ),
 
 fulfillments as (
     -- a raw consolidated fact table with three events that occur in sequence:
     -- packaged, shipped, delivered, in that order
     -- this table has a 1 to many relationship with orders
-    select * from fivetran.ecommerce.fulfillments
+    select 
+        timestamp, --added column names instead of *
+        orderid,
+        event_name,
+        agentid 
+    from {{ ref('bronze_fulfillments') }} --added reference
 ),
 
 agents as (
     -- a raw table with delivery agent details including agent fees, agents can be employees
     -- or contractors; all fulfillments have a related agent who processed the order at that phase
-    select * from fivetran.ecommerce.agents
+    select 
+       agentid, ----added column names instead of *
+       is_contractor,
+    from {{ ref('bronze_agents') }} --added reference
 ),
 
 date_spine as (
     -- a utility table that has one record for every day between today and 5 years ago
-    select * from analytics.dbt.util_days
+    select 
+       date_day --added col name instead of *
+    from analytics.dbt.util_days
 ),
 
 products as (
     -- a refined product dimension table
-    select * from analytics.dbt.dim_products
+    select 
+        products.product_id,  --added col names instead of *
+        products.product_name,
+        products.product_category,
+        products.product_subcategory,
+    from analytics.dbt.dim_products
 ),
 
 customers as (
     -- a refined customer dimension table
-    select * from analytics.dbt.dim_customers
+    select 
+        customer_id --added col names
+        country
+     from analytics.dbt.dim_customers
 ),
 
 joined as (
@@ -51,23 +78,26 @@ select
     min(shipped.agentid) as delivered_agentid
 from orders
 left join fulfillments as packaged
-on orders.orderid = packaged.order_id
+on orders.orderid = packaged.orderid
+and packaged.event_name = 'order_packaged' -- move condition to join for performance
 left join fulfillments as shipped
 on orders.orderid = shipped.order_id
+and shipped.event_name = 'order_shipped' --move condition to join for performance
 left join fulfillments as delivered
 on orders.orderid = delivered.order_id
-where packaged.event_name = 'order_packaged'
-and shipped.event_name = 'order_shipped'
-and delivered.event_name = 'order_delivered'
+and delivered.event_name = 'order_delivered' --move condition to join for performance
 -- set the grain to one record per order
 group by 1,2,3,4
 ),
 
 order_metrics as (
-select *, 
-    date_diff(createdat, packaged, day) as days_to_pack,
-    date_diff(packaged, shipped, day) as days_to_ship,
-    date_diff(createdat, delivered, day) as days_to_deliver,
+select 
+    joined.orderid,  --change * for col names
+    joined.productid,
+    joined.customerid,
+    date_diff(joined.createdat, joined.packaged, day) as days_to_pack, --added alias joined
+    date_diff(joined.packaged, joined.shipped, day) as days_to_ship, --added alias joined
+    date_diff(joined.createdat, join.delivered, day) as days_to_deliver, --added alias joined
     customers.country =  'United States' as is_us_customer, 
     -- determine if a contractor was used to fulfill the delivery
     packaged_agents.is_contractor or
@@ -91,21 +121,38 @@ final as (
         products.product_name,
         products.product_category,
         products.product_subcategory,
-        avg(days_to_pack) as avg_days_to_pack,
-        avg(days_to_ship) as avg_days_to_ship,
-        avg(days_to_deliver) as avg_days_to_deliver,
-        avg(case when is_us_customer then days_to_pack else null end) as avg_us_days_to_pack,
-        avg(case when is_us_customer then days_to_ship else null end) as avg_us_days_to_ship,
-        avg(case when is_us_customer then days_to_deliver else null end) as avg_us_days_to_deliver,
-        avg(case when has_contractor_support then days_to_pack else null end) as avg_contractor_days_to_pack,
-        avg(case when has_contractor_support then days_to_ship else null end) as avg_contractor_days_to_ship,
-        avg(case when has_contractor_support then days_to_deliver else null end) as avg_contractor_days_to_deliver
+        coalesce(avg(days_to_pack), 0) as avg_days_to_pack, --added coalesce to handle situation with no sales
+        coalesce(avg(days_to_ship), 0) as avg_days_to_ship,  --added coalesce to handle situation with no sales
+        coalesce(avg(days_to_deliver), 0) as avg_days_to_deliver  --added coalesce to handle situation with no sales
+         coalesce(avg(case when is_us_customer then days_to_pack else null end), 0) as avg_us_days_to_pack, --added coalesce to handle situation with no sales
+        coalesce(avg(case when is_us_customer then days_to_ship else null end), 0) as avg_us_days_to_ship, --added coalesce to handle situation with no sales
+        coalesce(avg(case when is_us_customer then days_to_deliver else null end), 0) as avg_us_days_to_deliver,  --added coalesce to handle situation with no sales
+        coalesce(avg(case when has_contractor_support then days_to_pack else null end), 0) as avg_contractor_days_to_pack,  --added coalesce to handle situation with no sales
+        coalesce(avg(case when has_contractor_support then days_to_ship else null end), 0) as avg_contractor_days_to_ship,  --added coalesce to handle situation with no sales
+        coalesce(avg(case when has_contractor_support then days_to_deliver else null end), 0) as avg_contractor_days_to_deliver,  --added coalesce to handle situation with no sales
+         {{ get_current_timestamp() }} as current_timestamp_utc-- adding the current UTC timestamp from macros
     from date_spine
-    cross join products
+    cross join products --cross join is good here, as we need to see all records, even when there were no products sold
     left join order_metrics
         on date_spine.date_day = date(order_metrics.createdat)
         and products.product_id = order_metrics.productid
     group by 1,2,3,4,5
 )
 
-select * from final
+select 
+    final.date_day,
+    final.product_id,
+    final.product_name,
+    final.product_category,
+    final.product_subcategory,
+    final.avg_days_to_pack,
+    final.avg_days_to_ship,
+    final.avg_days_to_deliver,
+    final.avg_us_days_to_pack,
+    final.avg_us_days_to_ship,
+    final.avg_us_days_to_deliver,
+    final.avg_contractor_days_to_pack,
+    final.avg_contractor_days_to_ship,
+    final.avg_contractor_days_to_deliver,
+    final.current_timestamp_utc --new column 
+ from final
